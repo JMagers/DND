a2346893ba2cc4e7f057939a5a4fc07540ca4b84
Jake Magers <jmagers1@mail.csuchico.edu>
2016-11-16 01:33:03 -0800

    Add 'other data' to React code

--- app/assets/javascripts/components/CharacterTemplate.js.jsx
+++ app/assets/javascripts/components/CharacterTemplate.js.jsx
@@ -13,7 +13,23 @@ var CharacterTemplate = React.createClass({
         <Traits data={data.traits} />
         <SavingThrows data={data.saving_throws} />
         <Skills data={data.skills} />
-        {/*Other data*/}
+        <h1>Other Data</h1>
+        <ul>
+          <li><strong>Name: </strong>{data.character_name}</li>
+          <li><strong>Class: </strong>{data.character_class}</li>
+          <li><strong>Background: </strong>{data.background}</li>
+          <li><strong>Race: </strong>{data.race}</li>
+          <li><strong>Alignment: </strong>{data.alignment}</li>
+          <li><strong>Passive Perception: </strong>{data.passive_perception}</li>
+          <li><strong>Armor Class: </strong>{data.armor_class}</li>
+          <li><strong>Initiative: </strong>{data.initiative}</li>
+          <li><strong>Speed: </strong>{data.speed}</li>
+          <li><strong>Other Proficiences/Languages: </strong>{data.other_proficiencies_languages}</li>
+          <li><strong>Hit Point Max: </strong>{data.hit_point_max}</li>
+          <li><strong>Hit Dice: </strong>{data.hit_dice}</li>
+          <li><strong>Features/Traits: </strong>{data.features_traits}</li>
+          <li><strong>Proficiency Bonus: </strong>{data.proficiency_bonus}</li>
+        </ul>
       </div>
     );
   }
