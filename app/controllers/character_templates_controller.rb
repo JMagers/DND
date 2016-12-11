cea083be94a7c7923c8441b53e1c8fe999a35dc2
Jake Magers <jmagers1@mail.csuchico.edu>
2016-12-10 17:55:45 -0800

    Add basic header

--- app/controllers/character_templates_controller.rb
+++ app/controllers/character_templates_controller.rb
@@ -1,4 +1,5 @@
 class CharacterTemplatesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_character_template, only: [:show, :edit, :update, :destroy]
 
   # FILTER
