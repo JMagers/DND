e7eb511200f176f7ad95c53cc4eddf15ac057005
Jake Magers <jmagers1@mail.csuchico.edu>
2016-12-10 22:09:44 -0800

    Add username to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validate :username_valid?
+
+  def username_valid?
+    username = self.username
+    if username == nil
+      self.errors.add(:username, "must not be nil")
+      return false
+    elsif username.length == 0
+      self.errors.add(:username, "must be included")
+    elsif User.exists?(username: username)
+      self.errors.add(:username, "has already been taken")
+    end
+  end
 end
