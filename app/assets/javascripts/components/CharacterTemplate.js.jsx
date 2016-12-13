48462ae72c73f4e2b978456bf80b1204a48bd8d4
Jake Magers <jmagers1@mail.csuchico.edu>
2016-12-13 01:24:06 -0800

    Fix 'Features/Traits' on show page

--- app/assets/javascripts/components/CharacterTemplate.js.jsx
+++ app/assets/javascripts/components/CharacterTemplate.js.jsx
@@ -10,7 +10,15 @@ var CharacterTemplate = React.createClass({
         <h1><u>{data.character_name}</u></h1>
         <h3>by <a href={"/character_templates?user_id=" + data.user_id}><strong>{data.username}</strong></a></h3>
         <p><a href={"/character_templates?user_id=" + data.user_id}>view all by {data.username}</a></p>
-        <p style={{marginTop: "1em"}}><strong><big>#Forks = {data.num_forks}</big></strong></p>
+        <div className="row" style={{margin: "8px 0px 8px 0px"}}>
+          <div className="shrink columns">
+            <div style={{border: "2px solid black"}}>
+              <div style={{paddingRight: "8px"}}>
+                <p style={{marginTop: "1em"}}><strong><big>#Forks: {data.num_forks}</big></strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="row">
           <div className="shrink columns">
             <ul>
@@ -25,12 +33,19 @@ var CharacterTemplate = React.createClass({
               <li><strong>Other Proficiences/Languages: </strong>{data.other_proficiencies_languages}</li>
               <li><strong>Hit Point Max: </strong>{data.hit_point_max}</li>
               <li><strong>Hit Dice: </strong>{data.hit_dice}</li>
-              <li><strong>Features/Traits: </strong>{data.features_traits}</li>
               <li><strong>Proficiency Bonus: </strong>{data.proficiency_bonus}</li>
             </ul>
           </div>
           <div className="columns">
             <img src={data.picture_url} />
+          </div>
+        </div>
+        <div className="row">
+          <div className="small-10 columns">
+            <h3>Features/Traits</h3>
+            <div className="callout primary">
+              <p>{data.features_traits}</p>
+            </div>
           </div>
         </div>
         <div className="row">
